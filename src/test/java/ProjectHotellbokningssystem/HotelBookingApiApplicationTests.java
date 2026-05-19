6cf868f8c707fba5b8807615037f9983a44eae52
Sukhawat <sukhawat.charoenchit@gmail.com>
2026-05-19 13:37:50 +0200

    first commit for Java project

---
+++ src/test/java/ProjectHotellbokningssystem/HotelBookingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ProjectHotellbokningssystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelBookingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
