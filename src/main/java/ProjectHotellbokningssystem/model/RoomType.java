8d4db10c79dc3a0372fc4710f86505e0c0c0f0f1
Sukhawat <sukhawat.charoenchit@gmail.com>
2026-05-28 18:30:53 +0200

    Roomtype structure Models

--- src/main/java/ProjectHotellbokningssystem/model/RoomType.java
+++ src/main/java/ProjectHotellbokningssystem/model/RoomType.java
@@ -1,4 +1,31 @@
 package ProjectHotellbokningssystem.model;
 
-public class RoomType {
+public enum RoomType {
+    SINGLE(500, 1, 10),
+    DOUBLE(1000, 2, 7),
+    FAMILY(1500, 5, 2),
+    SUITE(2000, 3, 3);
+
+
+    private final int price;
+    private final int maxCapacity;
+    private final int totalRooms;
+
+    RoomType(int price, int maxCapacity, int totalRooms){
+        this.price = price;
+        this.maxCapacity = maxCapacity;
+        this.totalRooms = totalRooms;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getMaxCapacity() {
+        return maxCapacity;
+    }
+
+    public int getTotalRooms() {
+        return totalRooms;
+    }
 }
